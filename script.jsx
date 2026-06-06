50aad632777e759e3543862a944b01209e7dcf32
AdhamMohamedKhairy <adhamfaied14@gmail.com>
2026-06-06 13:05:03 +0300

    HTML AND CSS changes + React

---
+++ script.jsx
@@ -0,0 +1,37 @@
+import {
+    useState, 
+    useEffect,
+    useRef
+} from "react";
+
+const BOXES = [
+    {
+        title: "Capstone Projects",
+        href: "cap.html",
+        photos: 8,
+    },
+
+    {
+        title: "Achievements",
+        href: "ach.html",
+        photos: 5,
+    },
+
+    {
+        title: "Life as a STEMer",
+        href: "stem.html",
+        photos: 10,
+    },
+
+    {
+        title: "Future Projects",
+        href: "fut.html",
+        photos: 4,
+    },
+
+    {
+        title: "Past and Current Projects",
+        href: "pandc.html",
+        photos: 7,
+    }
+]
